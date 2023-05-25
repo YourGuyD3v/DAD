24274aac482524bd5dff85f0d6de60f390f3d180
shurjeel khan <shurjeelkhan89@gmail.com>
2023-05-25 11:57:01 -0700

    Your commit message

--- contracts/TrackNFTData.sol
+++ contracts/TrackNFTData.sol
@@ -9,21 +9,24 @@ contract TrackNFTData is AccessControl, ChainlinkClient  {
         using Chainlink for Chainlink.Request;
 
         struct NftMetadata {
+            uint256 id;
+            address contrctAddress;
+            uint256 tokenId;
             string name;
             string symbol;
             string description;
-            string price;
             string imageUrl;
+            uint256 price;
             string createdAt;
-            string owner;
-            string isHidden;
-            string isFeatured;
+            address owner;
         }
 
     event RequestVolume(
         bytes32 indexed requestId,
         NftMetadata nftMetadata
     );
+
+    mapping(bytes32 => NftMetadata[]) internal i_nftMetadata;
 
     bytes32 public constant OWNER_ROLE = keccak256("OWNER_ROLE");
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
@@ -56,35 +59,37 @@ contract TrackNFTData is AccessControl, ChainlinkClient  {
             "get",
             i_apiUrl
         );
-        req.add("path", "collection,name");
-        req.add("path", "collection,name,description");
-        req.add("path", "collection,name,description,image_url");
-        req.add("path", "collection,name,description,image_url,total_supply,created_at");
-        req.add("path", "collection,name,description,image_url,total_supply,created_at,owner");
-        req.add("path", "collection,name,description,image_url,total_supply,created_at,owner,slug");
-
-        req.add("path", "listings,token_id");
-        req.add("path", "listings,token_id,owner,permalink,price");
-        req.add("path", "listings,token_id,owner,permalink,price,created_at,last_updated,is_sold,is_hidden");
-        req.add("path", "listings,token_id,owner,permalink,price,created_at,last_updated,is_sold,is_hidden,is_featured");
+        req.add("path", "items,id");
+        req.add("path", "items,id,asset_contract_address");
+        req.add("path", "items,id,asset_contract_address,token_id");
+        req.add("path", "items,id,asset_contract_address,token_id,name");
+        req.add("path", "items,id,asset_contract_address,token_id,name,description");
+        req.add("path", "items,id,asset_contract_address,token_id,name,description,image_url");
+        req.add("path", "items,id,asset_contract_address,token_id,name,description,image_url,price");
+        req.add("path", "items,id,asset_contract_address,token_id,name,description,image_url,price,created_at");
+        req.add("path", "items,id,asset_contract_address,token_id,name,description,image_url,price,created_at,updated_at,owner");
 
         return sendChainlinkRequest(req, fee);
     }
 
      function fulfill(
-        bytes32 _requestId, NftMetadata memory nftMetadata
+        bytes32 _requestId,
+         NftMetadata memory nftMetadataResponse,
+         bytes32[] memory _Data 
     ) public recordChainlinkFulfillment(_requestId) {
-        emit RequestVolume(_requestId, nftMetadata);
-        // nftMetadata.name = name;
+        emit RequestVolume(_requestId, nftMetadataResponse);
+        nftMetadataResponse.id = uint256(_Data[0]);
+        nftMetadataResponse.contrctAddress = address(uint160(uint256(_Data[1])));
+        nftMetadataResponse.tokenId = uint256(_Data[2]);
+        nftMetadataResponse.name = bytes32ToString(_Data[3]);
+        nftMetadataResponse.symbol = bytes32ToString(_Data[4]);
+        nftMetadataResponse.description = bytes32ToString(_Data[5]);
+        nftMetadataResponse.imageUrl = bytes32ToString(_Data[6]);
+        nftMetadataResponse.price = uint256(_Data[7]);
+        nftMetadataResponse.createdAt = bytes32ToString(_Data[8]);
+        nftMetadataResponse.owner = address(uint160(uint256(_Data[9])));
+
     }
-
-    function nftUrl() external {}
-
-    function nftImageUrl() external {}
-
-    function nftMetadata() external {}
-
-    function nftPrice() external {}
 
     function updateNftData(uint256 tokenId, string calldata newData ) external onlyRole(OWNER_ROLE)  {}
 
@@ -92,18 +97,35 @@ contract TrackNFTData is AccessControl, ChainlinkClient  {
 
     // View and Pure Functions
 
-    function getNftUrl() public view  returns (string memory) {}
+    function getNftImageUrl(uint256 tokenId) public view returns (string memory) {
+        // return string(abi.encodePacked(i_nftMetadata[tokenId].imageUrl));
+    }
 
-    function getImageUrl() public view  returns (string memory) {}
+    function getNftByAddress(address owner) public view returns (NftMetadata[] memory) {
+    bytes32 ownerBytes = bytes32(uint256(uint160(owner)));
+    return i_nftMetadata[ownerBytes];
 
-    function getNftMetadata() public view  returns (string memory) {}
+    }
 
     function getNftPrice() public view  returns (uint256) {}
 
     function getStoredNftData() public view  returns (bytes32) {}
 
-    function isAuthorized(address account) public view returns (bool) {
+    function isAuthorized(address account) public view returns (bool) { 
     return hasRole(ADMIN_ROLE, account);
-}
+
+    }
+
+    function bytes32ToString(bytes32 _bytes32) public pure returns (string memory) {
+        uint8 i = 0;
+        while(i < 32 && _bytes32[i] != 0) {
+            i++;
+        }
+        bytes memory bytesArray = new bytes(i);
+        for (i = 0; i < 32 && _bytes32[i] != 0; i++) {
+            bytesArray[i] = _bytes32[i];
+        }
+        return string(bytesArray);
+    }
 
 }
